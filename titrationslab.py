cc51f1976a01070152ffc671e2b13283956ca09b
Dan Feldheim <danfeldheim@gmail.com>
2019-07-16 11:50:00 -0600

    Added code to calculate [NaOH]
    Added code to calculate the molarity of NaOH from khp mass and volume values entered in standardization of NaOH tab.

--- titrationslab.py
+++ titrationslab.py
@@ -10,6 +10,7 @@ import os.path
 import glob, os
 import os.path
 from tkinter import messagebox
+import operator
 
 
 
@@ -26,10 +27,12 @@ class titrate:
         # Create tabs in root   
         notebook = ttk.Notebook(master, width = 600, height = 600)
         notebook.grid()
+        self.id = Frame(notebook, background = 'alice blue')
         self.naoh = Frame(notebook, background = 'alice blue')
         self.unknown = Frame(notebook, background = 'alice blue')
         self.calcs = Frame(notebook, background = 'alice blue')
 
+        notebook.add(self.id, text = "Student Identifiers")
         notebook.add(self.naoh, text = "Standardization of NaOH")
         notebook.add(self.unknown, text = "Unknown Acid Titration")
         notebook.add(self.calcs, text = "Calculations")
@@ -43,7 +46,34 @@ class titrate:
         self.style = ttk.Style(master)
         self.style.configure("TButton", font = ('times', 18), foreground = "red", highlightbackground = 'red')
         
-        # Create labels for Trial table
+        
+        # Create entries for student id
+        # self.id_Label = ttk.Label(self.id, text = 'Enter Student Identifiers', background = 'blue', foreground = "blue", 
+            # relief = RAISED, anchor = CENTER, width = 25,font = self.font1).grid(row = 0, column = 0, padx = 5, pady = (15, 5), sticky = 'e')
+        
+        self.last = StringVar()
+        self.last_label = ttk.Label(self.id, text = 'Last Name:', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 15, font = self.font2)
+        self.last_entry = ttk.Entry(self.id, width = 15, textvariable = self.last)
+        self.last_label.grid(row = 0, column = 0, pady = (10, 5))
+        self.last_entry.grid(row = 1, column = 0, pady = (1, 5))
+        self.last.set('Last Name') 
+        
+        self.first = StringVar()
+        self.first_label = ttk.Label(self.id, text = 'First Name:', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 15, font = self.font2)
+        self.first_entry = ttk.Entry(self.id, width = 15, textvariable = self.first)
+        self.first_label.grid(row = 0, column = 1, pady = (10, 5))
+        self.first_entry.grid(row = 1, column = 1, pady = (1, 5))
+        self.first.set('First Name') 
+        
+        self.student_id = StringVar()
+        self.student_id_label = ttk.Label(self.id, text = 'ID Number', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 15, font = self.font2)
+        self.student_id_entry = ttk.Entry(self.id, width = 15, textvariable = self.student_id)
+        self.student_id_label.grid(row = 2, column = 0, pady = (10, 5), columnspan = 1)
+        self.student_id_entry.grid(row = 3, column = 0, pady = (1, 5), columnspan = 1)
+        self.student_id.set('Student ID') 
+        
+        
+        # Create labels for standardization of NaOH table
         # Trial 1 label
         self.data_Label = ttk.Label(self.naoh, text = 'Data', background = 'blue', foreground = "blue", 
             relief = RAISED, anchor = CENTER, width = 10,font = self.font1).grid(row = 0, column = 0, padx = 5, pady = (15, 5))
@@ -68,50 +98,50 @@ class titrate:
             
         # Create entry boxes for data
         # Create entry box to display files selected for analysis
-        self.mass1 = IntVar()
+        self.mass1 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.mass1, width = 6)
         self.import_entry.grid(row = 1, column = 1, pady = (15, 5))
-        self.mass1.set('')
+        self.mass1.set('3.0')
         
-        self.mass2 = IntVar()
+        self.mass2 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.mass2, width = 6)
         self.import_entry.grid(row = 1, column = 2, pady = (15, 5))
-        self.mass2.set('')
+        self.mass2.set('6.0')
         
-        self.mass3 = IntVar()
+        self.mass3 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.mass3, width = 6)
         self.import_entry.grid(row = 1, column = 3, pady = (15, 5))
-        self.mass3.set('')
+        self.mass3.set('9.0')
         
-        self.init_vol1 = IntVar()
+        self.init_vol1 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.init_vol1, width = 6)
         self.import_entry.grid(row = 2, column = 1, pady = (15, 5))
-        self.init_vol1.set('')
+        self.init_vol1.set('1.0')
         
-        self.init_vol2 = IntVar()
+        self.init_vol2 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.init_vol2, width = 6)
         self.import_entry.grid(row = 2, column = 2, pady = (15, 5))
-        self.init_vol2.set('')
+        self.init_vol2.set('1.0')
         
-        self.init_vol3 = IntVar()
+        self.init_vol3 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.init_vol1, width = 6)
         self.import_entry.grid(row = 2, column = 3, pady = (15, 5))
-        self.init_vol1.set('')
+        self.init_vol3.set('1.0')
         
-        self.final_vol1 = IntVar()
+        self.final_vol1 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.final_vol1, width = 6)
         self.import_entry.grid(row = 3, column = 1, pady = (15, 5))
-        self.final_vol1.set('')
+        self.final_vol1.set('1.6')
         
-        self.final_vol2 = IntVar()
+        self.final_vol2 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.final_vol2, width = 6)
         self.import_entry.grid(row = 3, column = 2, pady = (15, 5))
-        self.final_vol2.set('')
+        self.final_vol2.set('2.5')
         
-        self.final_vol3 = IntVar()
+        self.final_vol3 = DoubleVar()
         self.import_entry = ttk.Entry(self.naoh, textvariable = self.final_vol3, width = 6)
         self.import_entry.grid(row = 3, column = 3, pady = (15, 5))
-        self.final_vol3.set('')
+        self.final_vol3.set('2.2')
         
         # Calculations tab
     
@@ -123,36 +153,131 @@ class titrate:
             relief = RAISED, anchor = CENTER, width = 20, font = self.font2).grid(row = 1, column = 1, columnspan = 1, pady = (15, 5))
             
         # Create entry boxes for molarities
-        self.M1 = IntVar()
+        self.M1 = DoubleVar()
         self.M1_label = ttk.Label(self.calcs, text = 'Trial 1', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 10, font = self.font2)
         self.M1_entry = ttk.Entry(self.calcs, width = 4, textvariable = self.M1)
         self.M1_label.grid(row = 2, column = 0, pady = (10, 5))
         self.M1_entry.grid(row = 3, column = 0, pady = (1, 5))
         self.M1.set(1)   
         
-        self.M2 = IntVar()
+        self.M2 = DoubleVar()
         self.M2_label = ttk.Label(self.calcs, text = 'Trial 2', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 10, font = self.font2)
         self.M2_entry = ttk.Entry(self.calcs, width = 4, textvariable = self.M2)
         self.M2_label.grid(row = 2, column = 1, pady = (10, 5))
         self.M2_entry.grid(row = 3, column = 1, pady = (1, 5))
         self.M2.set(1)  
             
-        self.M3 = IntVar()
+        self.M3 = DoubleVar()
         self.M3_label = ttk.Label(self.calcs, text = 'Trial 3', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 10, font = self.font2)
         self.M3_entry = ttk.Entry(self.calcs, width = 4, textvariable = self.M3)
         self.M3_label.grid(row = 2, column = 2, pady = (10, 5))
         self.M3_entry.grid(row = 3, column = 2, pady = (1, 5))
         self.M3.set(1)      
             
-        self.avg = IntVar()
+        self.avg = DoubleVar()
         self.avg_label = ttk.Label(self.calcs, text = 'Average Molarity', background = 'blue', foreground = "blue", relief = RAISED, anchor = CENTER, width = 15, font = self.font2)
         self.avg_entry = ttk.Entry(self.calcs, width = 4, textvariable = self.avg)
         self.avg_label.grid(row = 4, column = 1, pady = (10, 5))
         self.avg_entry.grid(row = 5, column = 1, pady = (1, 5))
         self.avg.set(1)  
+        
+        # Create button that calls function for selecting files for analysis
+        self.openFileButton = ttk.Button(self.calcs, text = 'Calculate', style = "TButton", command = lambda: self.get_naoh_molarity_values())
+        self.openFileButton.grid(row = 6, column = 1, pady = (1, 5))
+
             
+        # Function to calculate molarity of NaOH in the standardization of NaOH experiment
+    def get_naoh_molarity_values(self):
+        
+        try:
             
+            # Get data from trial 1-3 entry boxes
+            self.mass_khp1 = self.mass1.get()
+            self.mass_khp2 = self.mass2.get()
+            self.mass_khp3 = self.mass3.get()
+                
+            self.vi1 = self.init_vol1.get()
+            self.vi2 = self.init_vol2.get()
+            self.vi3 = self.init_vol3.get()
+                
+            self.vf1 = self.final_vol1.get()
+            self.vf2 = self.final_vol2.get()
+            self.vf3 = self.final_vol3.get()
+                
+            self.average = self.avg.get()
             
+            self.molarity1 = self.M1.get()
+            self.molarity2 = self.M2.get()
+            self.molarity3 = self.M3.get()
+            self.avg_molarity = self.avg.get()
+            
+            self.calc_naoh_molarity()
+            
+        except ValueError:
+            
+            messagebox.showinfo('Message', 'Please fill in all entry boxes.')
+            
+        
+    # Calculate average molarity based upon the khp masses and volumes entered in standardization of NaOH tab
+    def calc_naoh_molarity(self):
+        
+
+        # Create lists for values so calculation can be run in a loop
+        self.mass_khp_list = [self.mass_khp1, self.mass_khp2, self.mass_khp3]
+        self.vi_list = [self.vi1, self.vi2, self.vi3]
+        self.vf_list = [self.vf1, self.vf2, self.vf3]
+        
+        # Loop through lists to calculate moles of khp and molarity of NaOH
+        # Create lists for results
+        
+        self.moles_khp_list = []
+        self.molarity_naoh = []
+        
+        for mass in self.mass_khp_list:
+            self.moles_khp = mass/204.22
+            self.moles_khp_list.append(self.moles_khp)
+        
+        # Calculate average molarity and add to moles list
+        self.avg_moles_khp = sum(self.moles_khp_list)/3
+        self.moles_khp_list.append(self.avg_moles_khp)
+        
+        # Round number in list to hundredths place
+        self.rounded_moles_khp_list = [round(i, 2) for i in self.moles_khp_list]
+        
+        print ('Moles khp: ' + str(self.rounded_moles_khp_list))
+    
+        
+        # Calculate NaOH molarity for each trial and then average
+        # Calculate Vf - Vi for each element in vi_list and vf_list and add to list
+        self.vol_diff_list = map(operator.sub, self.vf_list, self.vi_list)
+        
+        print('Volume difference = ' + str(self.vol_diff_list))
+        
+        # Use zip to to grap corresponding elements from vol and moles lists and calculate
+        # Create a molarity list for results
+        self.naoh_molarity_list = []
+        for moles, vol in zip(self.moles_khp_list, self.vol_diff_list):
+            self.molarity = 1000*(moles/vol)
+            self.naoh_molarity_list.append(self.molarity)    
+                    
+        # Calculate average and append to list
+        self.avg_molarity = sum(self.naoh_molarity_list)/3
+        self.naoh_molarity_list.append(self.avg_molarity)
+        
+        print ('molarity = ' + str(self.naoh_molarity_list))
+        
+        # print (type(self.naoh_molarity_list[3]))
+        
+        # Round values to 2 decimal places
+        self.rounded_naoh_molarity_list = [round(i, 2) for i in self.naoh_molarity_list]
+            
+        print('Molarity = ' + str(self.rounded_naoh_molarity_list))
+       
+       
+                
+            
+        
+       
             
             
             
